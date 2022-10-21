7a44ec5a5a70de54d31eb603d0e68bcccaa6eb2a
Ørjan Gjengedal <og@datarespons.no>
2022-10-21 14:09:50 +0200

    awesome: rewrite ram widget

--- .config/awesome/widget/ram/init.lua
+++ .config/awesome/widget/ram/init.lua
@@ -4,105 +4,109 @@ local gears = require("gears")
 local watch = require("awful.widget.watch")
 local wibox = require("wibox")
 
+local ram = {}
 
-local ramgraph_widget = {}
+local function worker()
+  local timeout = 2
 
+  local memory = {
+    ["good"] = { bg = beautiful.bg_normal, fg = beautiful.fg_normal, symbol = "" },
+    ["bad"] = { bg = beautiful.bg_normal, fg = beautiful.fg_urgent, symbol = "" },
+    ["critical"] = { bg = beautiful.bg_normal, fg = beautiful.fg_critical, symbol = "" },
+  }
 
-local function worker(user_args)
-    local args = user_args or {}
-    local timeout = args.timeout or 1
-    local color_used = args.color_used or beautiful.bg_urgent
-    local color_free = args.color_free or beautiful.fg_normal
-    local color_buf  = args.color_buf  or beautiful.border_color_active
-    local widget_show_buf = args.widget_show_buf or false
-    local widget_height = args.widget_height or 25
-    local widget_width = args.widget_width or 25
+  local textbox = wibox.widget({
+    markup = memory["good"].symbol,
+    font = beautiful.font,
+    align = "center",
+    valign = "center",
+    widget = wibox.widget.textbox,
+  })
 
-    --- Main ram widget shown on wibar
-    ramgraph_widget = wibox.widget {
-        border_width = 0,
-        colors = {
-           color_used,
-           color_free,
-           color_buf,
-        },
-        display_labels = false,
-        forced_height = widget_height,
-        forced_width = widget_width,
-        widget = wibox.widget.piechart
-    }
+  --- Main ram widget shown on wibar
+  ram = wibox.widget({
+    textbox,
+    fg = memory["good"].fg,
+    bg = memory["good"].bg,
+    widget = wibox.widget.background,
+  })
 
-    --- Widget which is shown when user clicks on the ram widget
-    local popup = awful.popup{
-       ontop = true,
-       visible = false,
-       widget = {
-          widget = wibox.widget.piechart,
-          forced_height = 200,
-          forced_width = 400,
-          colors = {
-             color_used,
-             color_free,
-             color_buf,  -- buf_cache
-          },
-       },
-       shape = gears.shape.rounded_rect,
-       border_color = beautiful.border_color_active,
-       border_width = 1,
-       offset = { y = 5 },
-    }
+  --- Widget which is shown when user clicks on the ram widget
+  local popup = awful.popup({
+    ontop = true,
+    visible = false,
+    widget = {
+      widget = wibox.widget.piechart,
+      forced_height = 200,
+      forced_width = 400,
+      colors = {
+        beautiful.fg_critical, -- used
+        beautiful.fg_focus, -- free .. TODO: Change to green color
+        beautiful.fg_urgent, -- cache
+      },
+    },
+    shape = gears.shape.rounded_rect,
+    border_color = beautiful.border_color_active,
+    border_width = 1,
+    offset = { y = 5 },
+  })
 
-    --luacheck:ignore 231
-    local total, used, free, shared, buff_cache, available, total_swap, used_swap, free_swap
+  --luacheck:ignore 231
+  local total_used, total_free
+  local total, used, free, shared, buff_cache, available, total_swap, used_swap, free_swap
 
-    local function getPercentage(value)
-        return math.floor(value / (total+total_swap) * 100 + 0.5) .. '%'
+  local function getPercentage(value)
+    return math.floor((100 * value) / (total + total_swap) + 0.5)
+  end
+
+  watch('bash -c "LANGUAGE=en_US.UTF-8 free | grep -z Mem.*Swap.*"', timeout, function(widget, stdout)
+    total, used, free, shared, buff_cache, available, total_swap, used_swap, free_swap =
+    stdout:match("(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*Swap:%s*(%d+)%s*(%d+)%s*(%d+)")
+
+    total_used = used + used_swap
+    total_free = free + free_swap
+    local type
+    local free_percentage = getPercentage(total_free)
+    if free_percentage < 20 then
+      type = memory["critical"]
+    elseif free_percentage >= 20 and free_percentage < 40 then
+      type = memory["bad"]
+    else
+      type = memory["good"]
     end
 
-    watch('bash -c "LANGUAGE=en_US.UTF-8 free | grep -z Mem.*Swap.*"', timeout,
-        function(widget, stdout)
-            total, used, free, shared, buff_cache, available, total_swap, used_swap, free_swap =
-                stdout:match('(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*(%d+)%s*Swap:%s*(%d+)%s*(%d+)%s*(%d+)')
+    widget.widget.markup = type.symbol .. " " .. free_percentage .. "%"
+    widget.bg = type.bg
+    widget.fg = type.fg
 
-            if widget_show_buf then
-                widget.data = { used, free, buff_cache }
-            else
-                widget.data = { used, total-used }
-            end
+    if popup.visible then
+      popup:get_widget().data_list = {
+        { "used " .. getPercentage(total_used) .. "%", total_used },
+        { "free " .. getPercentage(total_free) .. "%", total_free },
+        { "cache " .. getPercentage(buff_cache) .. "%", buff_cache },
+      }
+    end
+  end, ram)
 
-            if popup.visible then
-               popup:get_widget().data_list = {
-                  {'used ' .. getPercentage(used + used_swap), used + used_swap},
-                  {'free ' .. getPercentage(free + free_swap), free + free_swap},
-                  {'buff_cache ' .. getPercentage(buff_cache), buff_cache}
-                }
-            end
-        end,
-        ramgraph_widget
-    )
+  ram:buttons(awful.util.table.join(awful.button({}, 1, function()
+    popup:get_widget().data_list = {
+      { "used " .. getPercentage(total_used) .. "%", total_used },
+      { "free " .. getPercentage(total_free) .. "%", total_free },
+      { "cache " .. getPercentage(buff_cache) .. "%", buff_cache },
+    }
 
-    ramgraph_widget:buttons(
-        awful.util.table.join(
-           awful.button({}, 1, function()
-                 popup:get_widget().data_list = {
-                    {'used ' .. getPercentage(used + used_swap), used + used_swap},
-                    {'free ' .. getPercentage(free + free_swap), free + free_swap},
-                    {'buff_cache ' .. getPercentage(buff_cache), buff_cache}
-                }
+    if popup.visible then
+      popup.visible = not popup.visible
+    else
+      popup:move_next_to(mouse.current_widget_geometry)
+    end
+  end)))
 
-                if popup.visible then
-                   popup.visible = not popup.visible
-                else
-                   popup:move_next_to(mouse.current_widget_geometry)
-                end
-            end)
-        )
-    )
-
-    return ramgraph_widget
+  return ram
 end
 
-
-return setmetatable(ramgraph_widget, { __call = function(_, ...)
+return setmetatable(ram, {
+  __call = function(_, ...)
     return worker(...)
-end })
+  end,
+})
