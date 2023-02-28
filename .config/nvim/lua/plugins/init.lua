01dbe2bcef2eb699dae58509907a4a616bf34bea
orjangj <orjan.gjengedal@gmail.com>
2023-02-28 21:52:51 +0100

    nvim: replace packer with lazy

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,24 +1,4 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Use a protected call so we don't error out on first use
-local status_ok, lazy = pcall(require, "lazy")
-if not status_ok then
-  return
-end
-
--- Install plugins here
-return lazy.setup({
+return {
   { "nvim-lua/plenary.nvim" }, -- Required by most plugins
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }, -- Required by most plugins
   { "nvim-treesitter/playground" },
@@ -143,4 +123,4 @@ return lazy.setup({
     },
     build = ":Neorg sync-parsers",
   },
-})
+}
