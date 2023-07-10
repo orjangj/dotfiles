e657c295c1e03e95eeebbb70197ef3bf63162a0e
orjangj <orjan.gjengedal@gmail.com>
2023-07-11 00:34:47 +0200

    hyprland: add locker script

--- .config/hypr/scripts/power_menu.sh
+++ .config/hypr/scripts/power_menu.sh
@@ -5,6 +5,8 @@ entries=" Lock\n⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
 selected=$(echo -e $entries | wofi -p "Power Menu" --width 270 --height 270 --dmenu --cache-file /dev/null --insensitive | awk '{print tolower($2)}')
 
 case $selected in
+  lock)
+    exec $HOME/.config/hypr/scripts/locker.sh ;;
   logout)
     hyprctl dispatch exit ;;
   suspend)
