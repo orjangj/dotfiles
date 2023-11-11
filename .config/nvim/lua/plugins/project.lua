3ed9225097aaa643f2f9dc7d8205746f12de46be
orjangj <orjan.gjengedal@gmail.com>
2023-11-11 21:16:09 +0100

    nvim: fix project.nvim config

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -2,7 +2,8 @@ return {
   "ahmedkhalf/project.nvim",
   config = function()
     require("project_nvim").setup({
-      pattern = { ">projects/git" },
+      detection_methods = { "pattern" },
+      patterns = { ">git" },
     })
 
     local tele_status_ok, telescope = pcall(require, "telescope")
