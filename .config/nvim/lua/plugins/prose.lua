f9dd2d7102dba6cc2206fd8908db1415f98288d7
orjangj <orjan.gjengedal@gmail.com>
2024-05-17 22:10:46 +0200

    nvim: update plugins to latest versions

--- .config/nvim/lua/plugins/prose.lua
+++ .config/nvim/lua/plugins/prose.lua
@@ -1,8 +1,14 @@
 return {
   -- {{{ Neorg
   {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
+  },
+  {
     "nvim-neorg/neorg",
     dependencies = {
+      { "vhyrro/luarocks.nvim" },
       { "folke/zen-mode.nvim" },
       { "folke/twilight.nvim" },
     },
