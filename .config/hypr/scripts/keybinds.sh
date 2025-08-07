019b22c1a966c4ae2abe652adb9e3a541aa6be13
orjangj <orjan.gjengedal@gmail.com>
2025-08-07 20:57:21 +0200

    hypr: remove unused script

--- .config/hypr/scripts/keybinds.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-keybinds=$(grep -oP '(?<=bind = ).*' $HOME/.config/hypr/hyprland.conf)
-keybinds=$(echo "$keybinds" | sed 's/,\([^,]*\)$/ = \1/' | sed 's/, exec//g' | sed 's/^,//g')
-wofi --dmenu -p "Keybinds" --insensitive <<< "$keybinds"
